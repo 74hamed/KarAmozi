d694c8eb4a41456af6d87202ad612a83fdc75649
Hamed <hamed.mn.93@gmail.com>
2024-06-29 14:03:21 +0330

    Smooth rotation

--- Sensor3DViewer/Sensor3DViewer/Form1.cs
+++ Sensor3DViewer/Sensor3DViewer/Form1.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Windows.Forms;
 using SharpGL;
-using SharpGL.SceneGraph.Assets;
 
 namespace Sensor3DViewer
 {
     public partial class Form1 : Form
     {
         private float roll, pitch, yaw;
+        private Timer renderTimer;
         private Timer sensorUpdateTimer;
         private OpenGLControl openGLControl;
+
         public Form1()
         {
             InitializeComponent();
             InitOpenGLControl();
+            InitRenderLoop();
             InitSensorSimulation();
         }
 
@@ -27,23 +29,33 @@ namespace Sensor3DViewer
             this.Controls.Add(openGLControl);
         }
 
+        private void InitRenderLoop()
+        {
+            renderTimer = new Timer();
+            renderTimer.Interval = 16; // 60 FPS
+            renderTimer.Tick += (sender, e) => openGLControl.Invalidate();
+            renderTimer.Start();
+        }
+
         private void InitSensorSimulation()
         {
             sensorUpdateTimer = new Timer();
-            sensorUpdateTimer.Interval = 450; // updating rate
+            sensorUpdateTimer.Interval = 5; // Update every 100 ms
             sensorUpdateTimer.Tick += SensorUpdateTimer_Tick;
             sensorUpdateTimer.Start();
         }
 
         private void SensorUpdateTimer_Tick(object sender, EventArgs e)
         {
-            // rotating the cube randomly (for now)
-            Random rand = new Random();
-            roll = (float)(rand.NextDouble() * 360);
-            pitch = (float)(rand.NextDouble() * 360);
-            yaw = (float)(rand.NextDouble() * 360);
 
-            openGLControl.Invalidate();
+            roll += 1.0f; // speed of rotation :
+            pitch += 0.5f;
+            yaw += 0.2f;
+
+            // values should be within 0-360 
+            roll = roll % 360;
+            pitch = pitch % 360;
+            yaw = yaw % 360;
         }
 
         private void OpenGLControl_OpenGLInitialized(object sender, EventArgs e)
@@ -66,21 +78,18 @@ namespace Sensor3DViewer
         {
             var gl = openGLControl.OpenGL;
 
+
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
             gl.LoadIdentity();
-      
+
+
             gl.Translate(0.0f, 0.0f, -5.0f);
+
 
             gl.Rotate(roll, 1.0f, 0.0f, 0.0f);
             gl.Rotate(pitch, 0.0f, 1.0f, 0.0f);
             gl.Rotate(yaw, 0.0f, 0.0f, 1.0f);
 
-            //texture mapping(https://stackoverflow.com/questions/71933324/sharpgl-low-resolution-textures)
-
-            //var texture = new SharpGL.SceneGraph.Assets.Texture();
-            //texture.Create(gl, "cratetexture.jpg");
-            //gl.Enable(OpenGL.GL_TEXTURE_2D);
-            //gl.BindTexture(OpenGL.GL_TEXTURE_2D, 0);
 
             DrawCube(gl);
 
@@ -89,7 +98,6 @@ namespace Sensor3DViewer
 
         private void DrawCube(OpenGL gl)
         {
-           
             gl.Begin(OpenGL.GL_QUADS);
 
             // Front 
@@ -136,15 +144,5 @@ namespace Sensor3DViewer
 
             gl.End();
         }
-
-        // Method to update the sensor data
-        public void UpdateSensorData(float newRoll, float newPitch, float newYaw)
-        {
-            roll = newRoll;
-            pitch = newPitch;
-            yaw = newYaw;
-            openGLControl.Invalidate(); 
-        }
-
     }
 }
